d1baf04d8cbd6977de4bc9f582e27a1fd4ec0d1d
grenadinio <grenadinio@yandex.ru>
2024-02-22 21:09:15 +0300

    Изменение и дополнение тестов под новую реализацию HistoryManager

--- test/service/InMemoryHistoryManagerTest.java
+++ test/service/InMemoryHistoryManagerTest.java
@@ -1,22 +1,46 @@
 package service;
 
 import model.Task;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("Менеджер истории")
 class InMemoryHistoryManagerTest {
+    static HistoryManager historyManager;
+
+    @BeforeEach
+    void init() {
+        historyManager = Managers.getDefaultHistory();
+    }
+
     @Test
-    @DisplayName("должен добавлять историю так, чтобы она не превышала 10")
-    void shouldAddHistoryNotMoreThen10Elements() {
-        InMemoryHistoryManager historyManager = new InMemoryHistoryManager();
-
-        for (int i = 1; i <= 15; i++) {
-            historyManager.add(new Task("Task " + i, "Task " + i));
+    @DisplayName("должен добавлять историю так, чтобы айди просмотренных задач были уникальны")
+    void shouldAddUniqueIDsInHistory() {
+        for (int i = 0; i <= 14; i++) {
+            historyManager.add(new Task(i, "Task " + i, TaskStatus.NEW, "Task " + i));
         }
+        historyManager.add(new Task(0, "Task " + 0, TaskStatus.NEW, "Task " + 0));
 
-        assertEquals(10, historyManager.getHistory().size(), "Размер истории превышает 10");
+        assertEquals(15, historyManager.getHistory().size(), "Неверный размер истории");
+        assertEquals(0, historyManager.getHistory().getLast().getId(), "Неверный ID последнего элемента истории");
+        assertEquals(1, historyManager.getHistory().getFirst().getId(), "Неверный ID первого элемента истории");
+    }
+
+    @Test
+    @DisplayName("должен удалять")
+    void should() {
+        for (int i = 0; i <= 14; i++) {
+            historyManager.add(new Task(i, "Task " + i, TaskStatus.NEW, "Task " + i));
+        }
+        historyManager.remove(0);
+        historyManager.remove(5);
+        historyManager.remove(14);
+
+        assertEquals(12, historyManager.getHistory().size(), "Неверный размер истории");
+        assertEquals(13, historyManager.getHistory().getLast().getId(), "Неверный ID последнего элемента истории");
+        assertEquals(1, historyManager.getHistory().getFirst().getId(), "Неверный ID первого элемента истории");
     }
 }
