ea1811706673fd40f6cfaddf04bf7b35b191cd52
grenadinio <grenadinio@yandex.ru>
2024-03-06 16:12:39 +0300

    Актуализирование дефолтного менеджера задач

--- src/service/Managers.java
+++ src/service/Managers.java
@@ -1,9 +1,14 @@
 package service;
 
+import service.history.HistoryManager;
+import service.history.InMemoryHistoryManager;
+import service.task.FileBackedTaskManager;
+import service.task.TaskManager;
+
 public class Managers {
 
     public static TaskManager getDefault() {
-        return new InMemoryTaskManager();
+        return new FileBackedTaskManager();
     }
 
     public static HistoryManager getDefaultHistory() {
